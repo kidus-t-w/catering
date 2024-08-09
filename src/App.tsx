86655e5fb4cd37fd13171694e425e3cbde8e8c3c
kidus-t-w <kidusk69@gmail.com>
2024-08-09 16:18:53 +0300

    Restarting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import GlobalStyle from "./globalStyles";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Navbar />
-      <div style={{ margin: '100px 0' }}> {/* Add margin-top equal to the height of the Navbar */}
-        <Outlet />
-      </div>  
-      <Footer />
+      <Outlet />
     </>
   );
 }
