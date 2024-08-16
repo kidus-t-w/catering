7e3a654942f1a63b8e73248ddaf6fb1aa6c4c2a0
kidus-t-w <kidusk69@gmail.com>
2024-08-16 15:03:09 +0300

    Modified Image size on mobile view

--- src/components/Events/Events.styles.ts
+++ src/components/Events/Events.styles.ts
@@ -58,11 +58,13 @@ export const EventImage = styled.img`
   border-radius: 50%;
 
   @media (max-width: 768px) {
-    max-width: 120px;
+    max-width: 100px;
+    max-height: 100px;
   }
 
   @media (max-width: 480px) {
     max-width: 100px;
+    max-height: 100px;
   }
 `;
 
