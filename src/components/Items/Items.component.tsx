f6b1b09c9c6be581866885c84769d3cb7e232a86
kidus-t-w <kidusk69@gmail.com>
2024-08-16 15:01:47 +0300

    Changed Images

--- src/components/Items/Items.component.tsx
+++ src/components/Items/Items.component.tsx
@@ -9,37 +9,37 @@ import {
 } from "./Items.styles";
 
 const items = [
-  { name: "Cheesy turnover", price: "$2.8", image: "/assets/items/item.png" },
+  { name: "Cup Cake", price: "$2.8", image: "/assets/items/cupcake.png" },
+  { name: "Muffin", price: "$4.6", image: "/assets/items/muffin.png" },
+  { name: "Pizza", price: "$2.8", image: "/assets/items/pizza.png" },
+  { name: "Cookie", price: "$2.8", image: "/assets/items/cookie.png" },
   {
-    name: "Buttery croissants",
+    name: "Fruit Punch",
     price: "$4.6",
-    image: "/assets/items/item.png",
+    image: "/assets/items/fruitPunch.png",
   },
-  { name: "Danish", price: "$2.8", image: "/assets/items/item.png" },
-  { name: "Eclair", price: "$2.8", image: "/assets/items/item.png" },
-  { name: "Cream puffs", price: "$4.6", image: "/assets/items/item.png" },
-  { name: "Mille-Feuille", price: "$2.8", image: "/assets/items/item.png" },
-  { name: "Fruit Tartlet", price: "$2.8", image: "/assets/items/item.png" },
-  { name: "Palmier", price: "$4.6", image: "/assets/items/item.png" },
-  { name: "Profiterole", price: "$2.8", image: "/assets/items/item.png" },
+  { name: "Cream Cake", price: "$2.8", image: "/assets/items/creamCake.png" },
+  { name: "Dry Cake", price: "$2.8", image: "/assets/items/dryCake.png" },
+  { name: "Baggel", price: "$4.6", image: "/assets/items/baggel.png" },
+  { name: "Donut", price: "$2.8", image: "/assets/items/donut.png" },
 ];
 
 const ItemsSection = () => {
   return (
     <>
-    <Heading>Our Products</Heading>
-    <ItemsContainer>
-      <ItemsGrid>
-        {items.map((item, index) => (
-          <ItemCard key={index}>
-            <ItemImage src={item.image} alt={item.name} />
-            <ItemInfo>
-              <ItemName>{item.name}</ItemName>
-            </ItemInfo>
-          </ItemCard>
-        ))}
-      </ItemsGrid>
-    </ItemsContainer>
+      <Heading>Our Products</Heading>
+      <ItemsContainer>
+        <ItemsGrid>
+          {items.map((item, index) => (
+            <ItemCard key={index}>
+              <ItemImage src={item.image} alt={item.name} />
+              <ItemInfo>
+                <ItemName>{item.name}</ItemName>
+              </ItemInfo>
+            </ItemCard>
+          ))}
+        </ItemsGrid>
+      </ItemsContainer>
     </>
   );
 };
--- src/components/Items/Items.styles.ts
+++ src/components/Items/Items.styles.ts
@@ -45,8 +45,13 @@ export const ItemCard = styled.div`
 `;
 
 export const ItemImage = styled.img`
-  width: 100%;
-  height: auto;
+object-fit: cover;  
+width: 100%;
+height: 300px;
+
+&:hover {
+  transform: scale(1.1);}
+  
 `;
 
 export const ItemInfo = styled.div`
