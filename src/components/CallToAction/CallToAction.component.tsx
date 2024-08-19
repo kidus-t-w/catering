f253025f7580c33b2bdd756586a9f1df48d79f57
kidus-t-w <kidusk69@gmail.com>
2024-08-19 12:47:42 +0300

    Changed button

--- src/components/CallToAction/CallToAction.component.tsx
+++ src/components/CallToAction/CallToAction.component.tsx
@@ -12,7 +12,7 @@ const CallToAction = () => {
         respective markets the AA members are supremely well suited to create a
         superb locally or regionally themed event.
       </CTAText>
-      <Button to='/contact_us'>Catering Requester</Button>
+      <Button to='/contact_us'>Catering Request</Button>
     </CTAContainer>
   );
 };
