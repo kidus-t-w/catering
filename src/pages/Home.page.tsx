af9aa17639f52f132f96009774b069ec65491f7c
kidus-t-w <kidusk69@gmail.com>
2024-08-19 12:49:59 +0300

    Added motion

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,10 +1,10 @@
 import CallToAction from "../components/CallToAction/CallToAction.component";
 import Events from "../components/Events/Events.component";
-// import EventTypes from "../components/EventTypes/EventTypes.component";
 import GetToKnowUs from "../components/GetToKnowUs/GetToKnowUs.component";
 import Hero from "../components/Hero/Hero.component";
 import Services from "../components/OurServices/Services.components";
 import Pictures from "../components/Pictures/Pictures.component";
+
 
 function Home() {
   return (
@@ -13,7 +13,6 @@ function Home() {
       <GetToKnowUs />
       <Services />
       <Events/>
-      {/* <EventTypes/> */}
       <Pictures/>
       <CallToAction/>
     </>
